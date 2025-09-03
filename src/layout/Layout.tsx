cbb72983fd86edd912ad5618b659f21557f8a5d0
KIMDJ\vzmzm <azmzm@naver.com>
2025-09-03 16:07:07 +0900

    [dj][2025-09-03] 메인 페이지 로드 딜레이, 모션 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
+import {useEffect, useState} from "react";
+import {AnimatePresence, motion} from "framer-motion";
 
 const Layout = () => {
+    const [loading, setLoading] = useState(true);
+
+    // 페이지 로드 시 딜레이
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="relative">
-            <Header />
-            <main>
-                <Outlet />
-            </main>
+            <AnimatePresence>
+                {loading ? (
+                    <div></div>
+                ) : (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1 }}
+                    >
+                        <Header />
+                        <Outlet />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
